5dcb7832e0535e64cf7a33f4d88f15d6c6d49675
Alaric Abyasa <alaricabyasa11@gmail.com>
2026-01-09 19:51:41 +0700

    allow all origins

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.options('/auth/challenge', cors({
 */
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://gymscribe.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-timezone');
   
